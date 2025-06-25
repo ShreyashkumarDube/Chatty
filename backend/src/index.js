9c55db3ff70421c1ac0a1b1dfce7932102e746c1
ShreyashkumarDube <86827868+ShreyashkumarDube@users.noreply.github.com>
2025-06-25 13:35:38 +0530

    update the authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import {connectDB} from './lib/db.js';
 const app = express();
@@ -8,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
