5052dca9d8e340b5f6041d510277c0919c828b58
ShreyashkumarDube <86827868+ShreyashkumarDube@users.noreply.github.com>
2025-06-25 13:15:55 +0530

    added the routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import {connectDB} from './lib/db.js';
 const app = express();
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on Port:"+ PORT);
+    connectDB();
 });
