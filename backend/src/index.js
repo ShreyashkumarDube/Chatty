fc52161e66a23403941e6b196e2f9a81df193795
ShreyashkumarDube <86827868+ShreyashkumarDube@users.noreply.github.com>
2025-06-25 15:18:38 +0530

    Refactor code structure for improved readability and maintainability

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
